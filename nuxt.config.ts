c7c1c5d2377796349db60e25f1cf94be5ee0869c
Johan Larsson <johan.larsson@ub.gu.se>
2026-01-27 13:33:27 +0100

    adds i18n support using I18n (Internationalization) module powered by Vue I18n.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/i18n"],
+  i18n: {
+    defaultLocale: "sv",
+    locales: [
+      { code: "en", name: "English", file: "en.json" },
+      { code: "sv", name: "Swedish", file: "sv.json" },
+    ],
+  },
+});
